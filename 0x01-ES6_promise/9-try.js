47781b8c3775e8f1be9f6e9349b75a2ce8165c91
Tim Simms <tsimms@gmail.com>
2022-05-04 20:56:37 -0500

    eslint

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,8 +1,7 @@
-
 export default function guardrail(mathFunction) {
-
-  try {return [mathFunction(), 'Guardrail was processed'];
-} catch (err) {
-  return [`Error: ${err.message}`, 'Guardrail was processed']
-}
+  try {
+    return [mathFunction(), 'Guardrail was processed'];
+  } catch (err) {
+    return [`Error: ${err.message}`, 'Guardrail was processed'];
+  }
 }
