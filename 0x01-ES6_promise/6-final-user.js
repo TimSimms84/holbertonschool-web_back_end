4568f75b75f3a394407a56ae79b5575e89186e4a
Tim Simms <tsimms@gmail.com>
2022-05-04 20:23:41 -0500

    task 6 -- not a test file of task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,6 +2,7 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default async function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((response) => response);
-}
+  return [ Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((response) => ({ status: 'fulfilled', value: response })).catch((response) => ({status: 'rejected', value: response.toString() }))
+  ];
+  }
