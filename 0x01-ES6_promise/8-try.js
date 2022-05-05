b8acbf00ca10a88fa349b95fdced6d4ca2c065a9
Tim Simms <tsimms@gmail.com>
2022-05-04 20:46:14 -0500

    eslint

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,8 +1,6 @@
 export default function divideFunction(numerator, denominator) {
   if (denominator !== 0) {
-    return (numerator / denominator)
-  }else{
-    throw Error('cannot divide by 0')
+    return (numerator / denominator);
   }
-
+  throw Error('cannot divide by 0');
 }
