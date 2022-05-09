1928b50687ae70312f955c7e3d12fbab62a14d47
Tim Simms <tsimms@gmail.com>
2022-05-09 16:43:52 -0500

    task 4

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,40 @@
+/* eslint no-underscore-dangle: ["error", { "allowAfterThis": true }] */
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof (amount) === 'number') {
+      this._amount = amount;
+    }
+    if (typeof (currency) === 'object') {
+      this._currency = currency;
+    }
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    if (typeof (currency) === 'object') {
+      this._currency = currency;
+    }
+  }
+
+  get amount() {
+    return this.amount;
+  }
+
+  set amount(amount) {
+    if (typeof (amount) === 'number') {
+      this._amount = amount;
+    }
+  }
+}
