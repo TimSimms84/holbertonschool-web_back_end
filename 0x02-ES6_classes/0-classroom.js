bfbbb3c12e225f9edc5f83fd82f096ad04a386dc
Tim Simms <tsimms@gmail.com>
2022-05-09 14:02:12 -0500

    task 0 and README

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,6 @@
+/* eslint no-underscore-dangle: ["error", { "allowAfterThis": true }] */
+export default class ClassRoom {
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
