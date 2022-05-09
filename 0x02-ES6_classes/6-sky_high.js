512da0532554f5d103e567279ee6d273aa3cd280
Tim Simms <tsimms@gmail.com>
2022-05-09 17:11:04 -0500

    task 6

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,21 @@
+/* eslint no-underscore-dangle: ["error", { "allowAfterThis": true }] */
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  set floors(floors) {
+    this._floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
+}
