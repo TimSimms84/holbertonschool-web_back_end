ec4bb11dbdd8b8f5096fb619ba8614d67b2bbe80
Tim Simms <tsimms@gmail.com>
2022-05-09 17:00:24 -0500

    task 5

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,16 @@
+/* eslint no-underscore-dangle: ["error", { "allowAfterThis": true }] */
+
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+    if (!this.evacuationWarningMessage) throw Error('Class extending Building must override evacuationWarningMessage');
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(sqft) {
+    this._sqft = sqft;
+  }
+}
