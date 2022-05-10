4cb5008f4d342543dbb773f0008e56b295431af4
Tim Simms <tsimms@gmail.com>
2022-05-10 18:03:08 -0500

    task 4

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,11 @@
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  if (!Array.isArray(students)) {
+    return 0;
+  }
+  return students.filter((student) => student.location === city).map((student) => {
+    const grades = newGrades.find((grade) => grade.studentId === student.id);
+    const grade = grades ? grades.grade : 'N/A';
+    return { ...student, grade };
+  })
+
+}
