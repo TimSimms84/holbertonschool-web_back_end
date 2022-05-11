e2bd4a3c55f665c81a73b233be2fcc05435ae1d0
Tim Simms <tsimms@gmail.com>
2022-05-11 13:39:13 -0500

    eslint compliance

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -5,5 +5,5 @@ export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
   const int8view = new Int8Array(buffer);
   int8view[position] = value;
-  return new DataView(buffer, int8view)
+  return new DataView(buffer, int8view);
 }
