e6b91be5d2b03d0061d34989594378fe6a30cb5a
Tim Simms <tsimms@gmail.com>
2022-09-19 13:19:14 -0500

    push all task; works locally

---
+++ 0x03-Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+""" class to manage the API authentication. """
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth():
+    """
+    api Auth
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Returns True if path is None
+        Returns True if excluded_paths is None or empty
+        Returns False if path is in excluded_paths
+        You can assume excluded_paths contains string path always ending by a /
+        This method must be slash tolerant: path=/api/v1/status and
+        path=/api/v1/status/ must be returned False if excluded_paths
+        contains /api/v1/status/
+        """
+        if not path:
+            return True
+        if path[-1] != '/':
+            path += '/'
+        if path in excluded_paths:
+            return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """
+        request is None, returns None
+        If request doesn’t contain the header key Authorization, returns None
+        Otherwise, return the value of the header request Authorizatio
+        """
+        if request is None:
+            return None
+        if not request.headers.get("Authorization"):
+            return None
+        return request.headers.get("Authorization")
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        return None
