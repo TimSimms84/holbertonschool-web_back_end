08fd5e24e20ec1136e6b1901c6237f4a87276f54
Tim Simms <tsimms@gmail.com>
2022-09-26 16:53:46 -0500

    change imports for older python

--- 0x05-user_authentication_service/user.py
+++ 0x05-user_authentication_service/user.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ User model """
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 Base = declarative_base()
 
