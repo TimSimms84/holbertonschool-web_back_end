dea6a255505af6e295f9b41e0e8ea7835410d041
Tim Simms <tsimms@gmail.com>
2022-10-05 14:25:07 -0500

    task 3

---
+++ 0x01-i18n/3-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""basic flask app"""
+
+import babel
+from flask import Flask, render_template, request
+from flask_babel import _, Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """Config class"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+_(u'home_title')
+_(u'home_header')
+
+
+@babel.localeselector
+def get_locale():
+    """get locale"""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """index page"""
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000', debug=True)
