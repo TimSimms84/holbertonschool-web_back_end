3c3a1ceb5416aade63ec27c4ea8d35d89ce3930b
Tim Simms <tsimms@gmail.com>
2022-10-05 12:46:22 -0500

    pycodestyle

--- 0x01-i18n/2-app.py
+++ 0x01-i18n/2-app.py
@@ -8,18 +8,22 @@ from flask_babel import Babel
 app = Flask(__name__)
 babel = Babel(app)
 
+
 class Config(object):
     """Config class"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = 'en'
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
+
 app.config.from_object(Config)
+
 
 @babel.localeselector
 def get_locale():
     """get locale"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def index():
