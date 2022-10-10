9c1c1d230858522a4f8fd33b8d4bd87522d003eb
Tim Simms <tsimms@gmail.com>
2022-10-10 16:42:42 -0500

    cleanup

--- 0x01-i18n/7-app.py
+++ 0x01-i18n/7-app.py
@@ -4,7 +4,7 @@
 
 from flask import Flask, render_template, request, g
 from flask_babel import Babel
-from pytz import timezone, UnknownTimeZoneError, all_timezones
+from pytz import timezone, UnknownTimeZoneError
 
 
 app = Flask(__name__)
