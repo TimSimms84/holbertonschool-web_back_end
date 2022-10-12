785a0e20d1077f7c9f96cc720d2fe101cabe4e5a
Tim Simms <tsimms@gmail.com>
2022-10-12 14:24:05 -0500

    corrections user_id = user_id

--- 0x01-MySQL_Advanced/7-average_score.sql
+++ 0x01-MySQL_Advanced/7-average_score.sql
@@ -1,12 +1,13 @@
 -- creates stored procedure ComputeAverageScoreForuser that computes the average score for a student
 
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
 DELIMITER $$
-CREATE PROCEDURE ComputeAverageScoreForuser(
-	in user_id int
-)
+CREATE PROCEDURE ComputeAverageScoreForUser(
+    IN user_id INT)
 BEGIN
-	DECLARE avg_score float;
-	set avg_score = (SELECT AVG(score) FROM corrections WHERE user_id = user_id);
-	UPDATE users SET average_score = avg_score WHERE id = user_id;
-END $$
+    DECLARE avg_score FLOAT;
+    SET avg_score = (SELECT AVG(score) FROM corrections as c WHERE c.user_id=user_id);
+    UPDATE users SET average_score = avg_score WHERE id=user_id;
+END
+$$
 DELIMITER ;
