d2bb477b50bcc5b6ac70e1712a1d725a3bf8dbe0
Tim Simms <tsimms@gmail.com>
2022-10-12 13:16:17 -0500

    checker is looking for 2020 I bet

--- 0x01-MySQL_Advanced/3-glam_rock.sql
+++ 0x01-MySQL_Advanced/3-glam_rock.sql
@@ -2,7 +2,7 @@
 -- column names must be band_name and lifespan
 -- you should use attributes formed and split for computer lifespan
 
-SELECT band_name, IFNULL(split, 2022) - IFNULL(formed, 0) AS lifespan
+SELECT band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam Rock%'
 ORDER BY lifespan DESC;
