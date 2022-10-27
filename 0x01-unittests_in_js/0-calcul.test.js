b73b7e7a41d1d684022252da6304b9baa6b986cb
Tim Simms <tsimms@gmail.com>
2022-10-27 12:15:50 -0500

    testing

--- 0x01-unittests_in_js/0-calcul.test.js
+++ 0x01-unittests_in_js/0-calcul.test.js
@@ -5,20 +5,20 @@ const calculateNumber = require('./0-calcul');
 describe('calculateNumber', () => {
   // eslint-disable-next-line jest/prefer-expect-assertions
   it('postive intergers', () => {
-    assert.equal(calculateNumber(1, 3), 4);
-    assert.equal(calculateNumber(1, 3.7), 5);
-    assert.equal(calculateNumber(1.2, 3.7), 5);
-    assert.equal(calculateNumber(1.5, 3.7), 6);
-    assert.equal(calculateNumber(3.7, 1), 5);
-    assert.equal(calculateNumber(3.7, 1.2), 5);
+    assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    assert.strictEqual(calculateNumber(3.7, 1), 5);
+    assert.strictEqual(calculateNumber(3.7, 1.2), 5);
   });
   // eslint-disable-next-line jest/prefer-expect-assertions
   it('negative intergers', () => {
-    assert.equal(calculateNumber(-1, -3), -4);
-    assert.equal(calculateNumber(-1, -3.7), -5);
-    assert.equal(calculateNumber(-1.2, -3.7), -5);
-    assert.equal(calculateNumber(-1.6, -3.7), -6);
-    assert.equal(calculateNumber(-3.7, -1), -5);
-    assert.equal(calculateNumber(-3.7, -1.2), -5);
+    assert.strictEqual(calculateNumber(-1, -3), -4);
+    assert.strictEqual(calculateNumber(-1, -3.7), -5);
+    assert.strictEqual(calculateNumber(-1.2, -3.7), -5);
+    assert.strictEqual(calculateNumber(-1.6, -3.7), -6);
+    assert.strictEqual(calculateNumber(-3.7, -1), -5);
+    assert.strictEqual(calculateNumber(-3.7, -1.2), -5);
   });
 });
