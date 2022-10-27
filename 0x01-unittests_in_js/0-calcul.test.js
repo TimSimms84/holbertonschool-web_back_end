0f2ac24f08270770528b1d804e04fc152468dc7e
Tim Simms <tsimms@gmail.com>
2022-10-27 12:23:50 -0500

    testing again

--- 0x01-unittests_in_js/0-calcul.test.js
+++ 0x01-unittests_in_js/0-calcul.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable jest/prefer-expect-assertions */
 /* eslint-disable jest/expect-expect */
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
@@ -20,5 +21,19 @@ describe('calculateNumber', () => {
     assert.strictEqual(calculateNumber(-1.6, -3.7), -6);
     assert.strictEqual(calculateNumber(-3.7, -1), -5);
     assert.strictEqual(calculateNumber(-3.7, -1.2), -5);
+  });
+
+  it('returns NaN if a or b is infinite', () => {
+    assert.strictEqual(calculateNumber(Infinity, 1.6), Infinity);
+    assert.strictEqual(calculateNumber(1.6, Infinity), Infinity);
+    assert.strictEqual(calculateNumber(Infinity, Infinity), Infinity);
+    assert.strictEqual(calculateNumber(-Infinity, 1.6), -Infinity);
+    assert.strictEqual(calculateNumber(1.6, -Infinity), -Infinity);
+    assert.strictEqual(calculateNumber(-Infinity, -Infinity), -Infinity);
+  });
+  it('returns NaN if a or b is not a number', () => {
+    assert.strictEqual(calculateNumber('A', 1.6), NaN);
+    assert.strictEqual(calculateNumber(1.6, 'B'), NaN);
+    assert.strictEqual(calculateNumber('A', 'B'), NaN);
   });
 });
